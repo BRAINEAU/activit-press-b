1f885c61c31d5fd2af6f9065257c607ba35de479
BRAINEAU <107110887+BRAINEAU@users.noreply.github.com>
2022-09-05 12:10:26 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+    music.playMelody("C5 B A G F E D C ", 188)
+})
+basic.showString("Bonjour!")
+basic.showString("B")
+basic.showArrow(ArrowNames.East)
